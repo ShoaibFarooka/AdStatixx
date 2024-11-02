f0f3eef620fdda1a2e0908d9467447f722e84197
Shoaib Farooka <shoaibfarooka@gmail.com>
2024-11-02 18:59:53 +0500

    company api's for campaigns done

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const UserRoutes = require('./userRoutes');
+const campaignRoutes = require('./campaignRoutes');
 
 const router = express.Router();
 
 // Set up routes
 router.use('/user', UserRoutes);
+router.use('/campaign', campaignRoutes);
 
 module.exports = router;
