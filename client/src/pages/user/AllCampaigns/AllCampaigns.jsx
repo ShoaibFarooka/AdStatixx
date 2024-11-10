700a7d70ce686b42ae5f796219264e106266fe60
Bilal <bilaldeveloper900@gmail.com>
2024-11-10 13:05:08 +0500

    second screen doen

--- client/src/pages/user/AllCampaigns/AllCampaigns.jsx
+++ client/src/pages/user/AllCampaigns/AllCampaigns.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../../../assets/images/campaign.svg'
+import plan from "../../../assets/images/company.svg"
+import back from "../../../assets/icons/back.svg"
 
-const CampaignCard = () => {
+const CampaignCard = ({ setSeeViewMore }) => {
   const truncateText = (text, wordLimit) => {
     const words = text.split(' ');
     return words.length > wordLimit ? words.slice(0, wordLimit).join(' ') + '...' : text;
@@ -20,7 +22,7 @@ const CampaignCard = () => {
         </div>
       </div>
 
-     <p className="text-[13px] font-medium mt-[17px] leading-4 h-[79px]">
+      <p className="text-[13px] font-medium mt-[17px] leading-4 h-[79px]">
         {truncateText(description, 35)}
       </p>
 
@@ -35,7 +37,7 @@ const CampaignCard = () => {
         </div>
       </div>
 
-      <button className="bg-[#6AB541] text-white font-bold w-full py-2 mt-4 mb-4 rounded-lg hover:bg-[#72c446] transition duration-300">
+      <button onClick={() => setSeeViewMore(false)} className="bg-[#6AB541] text-white font-bold w-full py-2 mt-4 mb-4 rounded-lg hover:bg-[#72c446] transition duration-300">
         View More
       </button>
     </div>
@@ -47,12 +49,69 @@ const CampaignGrid = () => {
     description: "Rorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis."
   });
 
+  const [seeViewMore, setSeeViewMore] = useState(true)
+
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-6 sm:p-4">
-      {campaigns.map((campaign, index) => (
-        <CampaignCard key={index} description={campaign.description} />
-      ))}
-    </div>
+    <>
+      {seeViewMore ? <>
+        <div className="flex mt-4 sm:mb-2">
+          <img src={plan} alt="plan" className='mr-3' />
+
+          <span>Add Campaign</span>
+        </div>
+
+        <div className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-6 sm:p-4">
+          {campaigns.map((campaign, index) => (
+            <CampaignCard key={index} description={campaign.description} setSeeViewMore={setSeeViewMore} />
+          ))}
+        </div>
+      </> : <>
+        <div className="flex mt-4 mb-4 items-center cursor-pointer" onClick={() => setSeeViewMore(true)}>
+          <img src={back} alt="back" className='mr-3' />
+
+          <span className='text-xl font-bold'>Back</span>
+        </div>
+
+        <div className='pt-[61px] pl-[48px] pr-[36px] pb-[48px] bg-white rounded-2xl'>
+          <div className='flex'>
+            <div>
+              <img src={logo} alt="back" className='mr-9 w-[141px] h-[141px] rounded-full' />
+            </div>
+
+            <div className='max-w-[824px]'>
+              <span className=' font-bold text-xl'>Campaign Name</span>
+
+              <p className='pt-6'>Rorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.Rorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.Rorem ipsum dolor sit amet.</p>
+
+
+              <div className="flex mt-[21px] text-center">
+                <div className='mr-6'>
+                  <p className="font-semibold text-sm ">Remaining Budget</p>
+                  <p className="w-[117px] h-[30px] rounded-[5px] bg-[#F8F8F8] flex justify-center items-center font-normal text-[13px] mt-2">$200</p>
+                </div>
+                <div>
+                  <p className="font-semibold text-sm ">Budget Per View</p>
+                  <p className="w-[117px] h-[30px] rounded-[5px] bg-[#F8F8F8] flex justify-center items-center font-normal text-[13px] mt-2">$50</p>
+                </div>
+              </div>
+
+              <div className='flex mt-7'>
+                <button className='bg-[#6AB541] py-[18px] w-[235px] rounded-[10px] text-white font-bold mr-5'>
+                  Accept Campaign
+                </button>
+
+                <button className='border border-[#6AB541] text-[#6AB541] py-[18px] w-[235px] rounded-[10px]'>
+                  Download Assets
+                </button>
+              </div>
+            </div>
+
+
+          </div>
+        </div>
+      </>
+      }
+    </>
   );
 };
 
